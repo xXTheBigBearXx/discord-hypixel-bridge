bcc676085954ff7a65869b7f4822d997eb955569
Nora <notsquag@gmail.com>
2019-11-09 00:03:21 -0500

    Technically works now.
    Added core functionality. Still lots to do but at least it does technically work.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mineflayer = require("mineflayer");
 const discord = require("discord.js");
 const config = require("./config.json");
-const colours = require("colors");
+require("colors");
 
+const client = new discord.Client({autoReconnect: true});
 const options = {
     host: 'mc.hypixel.net',
     port: 25565,
@@ -10,6 +11,8 @@ const options = {
     username: config["minecraft-username"],
     password: config["minecraft-password"],
 };
+
+// minecraft bot stuff vv
 let mc;
 (function init() {
     console.log("Logging in.");
@@ -36,11 +39,47 @@ mc.on("login", () => {
 });
 
 mc.on("message", (chatMsg) => {
-    const msg = chatMsg.text;
+    const msg = chatMsg.toString();
     console.log("Minecraft: ".brightGreen + msg);
-    if (msg.endsWith(" joined the lobby!")) {
+    if (msg.endsWith(" joined the lobby!") && msg.includes("[MVP+")) {
         console.log("Sending to limbo.");
         mc.chat("/achat \u00a7ca");
         return;
     }
+
+    if (msg.startsWith("Guild >") && msg.includes(":")) {
+        let v = msg.split(" ", 2);
+        if (v[2].includes(name + ":") || v[3].includes(name + ":")) return;
+
+        let splitMsg = msg.split(" ");
+        let i = msg.indexOf(":");
+        let splitMsg2 = [msg.slice(0,i), msg.slice(i+1)];
+        let sender, sentMsg;
+        if (splitMsg[2].includes("[")) {
+            sender = splitMsg[3].replace(":","");
+        } else {
+            sender = splitMsg[2].replace(":","");
+        }
+        sentMsg = splitMsg2[1];
+
+        let embed = new discord.RichEmbed()
+            .setAuthor(sender + ": " + sentMsg, "https://www.mc-heads.net/avatar/" + sender)
+            .setColor("GREEN");
+
+        //channel.send(embed);
+        client.guilds.get(config["discord-guild"]).channels.get(config["discord-channel"]).send(embed);
+    }
 });
+
+// discord bot stuff vv
+client.on("ready", () => {
+    console.log("Discord: Logged in.".bgBlue);
+});
+
+client.on("message", (message) => {
+    if (message.channel.id !== config["discord-channel"] || message.author.bot || message.content.startsWith(config["discord-bot-prefix"])) return;
+    console.log("Discord: ".blue + message.author.username + ": " + message.content);
+    mc.chat("/gc d. " + message.author.username.replace(" ", "") + ": " + message.content);
+});
+
+client.login(config["discord-token"]);
