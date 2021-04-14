78a5080ecff0caa79b750c7f684b9ac11bb4a34a
Jay <jaystmaster@gmail.com>
2021-04-14 02:45:53 +0100

    Update index.js
    Added guild member counting in activity.
    Cleaned up some parts of code.
    Made use of switch/case.

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ const options = {
 
 // Minecraft Bot
 let mc;
+var currentPlayers = 0;
+var onlineMembers = 0;
 
 mc = mineflayer.createBot(options);
 mc._client.once("session", session => options.session = session);
 mc.once("end", () => {
-        console.log("Connection failed.");
-        process.exit(0);
+    console.log("Connection failed.");
+    process.exit(0);
 });
 
 mc.on("login", () => {
@@ -32,63 +34,93 @@ mc.on("login", () => {
     }, 1000);
     setTimeout(() => {
         console.log("Switching to guild chat. (If not already.)");
-          mc.chat("/chat g");
+        mc.chat("/chat g");
     }, 2000);
-     mc.chat("Logged in")
+    setTimeout(() => {
+        mc.chat("Logged in");
+    }, 3000);
+    setTimeout(() => {
+        mc.chat("/g online");
+    }, 4000);
 });
+
+
 
 mc.on("message", (chatMsg) => {
     const msg = chatMsg.toString();
     console.log("Minecraft: ".brightGreen + msg);
-    if (msg.endsWith(" joined the lobby!") && msg.includes("[MVP+")) {
-        console.log("Sending to limbo.");
-        mc.chat("/achat \u00a7ca");
-        return;
-    }
+
+    let msgParts = msg.split(" ");
 
     if (msg.startsWith("Guild >")) {
-        let msgParts = msg.split(" ");
         if (msgParts[2].includes(mc.username) || msgParts[3].includes(mc.username)) return;
         if (msgParts.length == 4 && !msg.includes(":")) {
             client.guilds.get(config["discord-guild"]).channels.get(config["chat-channel"]).sendMessage(msgParts[2] + " " + msgParts[3]);
+            switch (msgParts[3]) {
+                case "joined.":
+                    onlineMembers++
+                    break;
+                case "left.":
+                    --onlineMembers
+                    break;
+            }
         } else {
-        let i = msg.indexOf(":");
-        let sentMsg = [msg.slice(0, i), msg.slice(i + 1)];
-        let sender;
-        if (msgParts[2].includes("[")) {
-            sender = msgParts[3].replace(":", "");
+            let i = msg.indexOf(":");
+            let sentMsg = [msg.slice(0, i), msg.slice(i + 1)];
+            let sender;
+            if (msgParts[2].includes("[")) {
+                sender = msgParts[3].replace(":", "");
+            } else {
+                sender = msgParts[2].replace(":", "");
+            }
+
+            let embed = new discord.RichEmbed()
+                .setAuthor(sender + ": " + sentMsg[1], "https://www.mc-heads.net/avatar/" + sender)
+                .setColor("GREEN");
+
+            client.guilds.get(config["discord-guild"]).channels.get(config["chat-channel"]).send(embed);
+        }
+    }
+
+    if (msg.startsWith("Online Members")) {
+        onlineMembers = msgParts[2];
+    }
+
+    if (onlineMembers !== currentPlayers) {
+        client.user.setPresence({
+            status: "online", //You can show online, idle....
+            game: {
+                name: onlineMembers + " guild members", //The message shown
+                type: "WATCHING" //PLAYING: WATCHING: LISTENING: STREAMING:
+            }
+        });
+        currentPlayers = onlineMembers
+    }
+
+    // Join/Leave Messages
+    if (msg.includes("the guild") && !msg.includes(":")) {
+        var i;
+        if (msg.startsWith("[")) {
+            i = 1;
         } else {
-            sender = msgParts[2].replace(":", "");
+            i = 0;
         }
 
-        let embed = new discord.RichEmbed()
-            .setAuthor(sender + ": " + sentMsg[1], "https://www.mc-heads.net/avatar/" + sender)
-            .setColor("GREEN");
-
-
-        client.guilds.get(config["discord-guild"]).channels.get(config["chat-channel"]).send(embed);
-    }}
-        // Join/Leave Messages
-    if (msg.endsWith("the guild!")) {
-            let msgParts = msg.split(" ");
-            var i;
-            if (msg.includes(":")) return;
-            if (msg.startsWith("[")) {
-                i = 1;
-            } else {
-                i = 0;
-            }
-    
-            if (msgParts[i + 1] == "joined") {
-                console.log(msgParts[i])
+        switch (msgParts[i + 1]) {
+            case "joined":
                 client.guilds.get(config["discord-guild"]).channels.get(config["log-channel"]).sendMessage(msgParts[i] + " joined the guild.");
                 mc.chat("Welcome " + msgParts[i] + "!");
-            } else {
-                console.log(msgParts[i])
+                break;
+            case "left":
                 client.guilds.get(config["discord-guild"]).channels.get(config["log-channel"]).sendMessage(msgParts[i] + " left the guild.");
-                mc.chat("F")
-            } 
+                mc.chat("F");
+                break;
+            case "was": // [Rank] Name was kicked from the guild by [Rank] Name!
+                client.guilds.get(config["discord-guild"]).channels.get(config["log-channel"]).sendMessage(msgParts[i] + " was kicked from the guild.");
+                mc.chat("L");
+                break;
         }
+    }
 });
 
 // Discord Bot
